a56b2d4d12ecac2bb6bad2f7009a70ea7ca8141f
sean.robb <sean.robb@centurylink.com>
2015-08-29 13:15:26 -0500

    Now height must be smaller then 23

--- Java/src/main/java/Pyramid.java
+++ Java/src/main/java/Pyramid.java
@@ -6,7 +6,7 @@ public class Pyramid {
     private int height;
 
     public Pyramid(int height) throws Exception {
-        if (height > 0) {
+        if (height > 0 && height < 24) {
             this.height = height;
         } else {
             throw new Exception("The pyramid height must be a non negative number and no greater then 23.");
