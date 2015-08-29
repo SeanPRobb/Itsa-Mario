629aea70fe618e8db17173d2bb30bc872474897b
sean.robb <sean.robb@centurylink.com>
2015-08-29 13:48:56 -0500

    Now the toString method will print out a string with a half pyramid shape in hashes with the height that you initalize the pyramid with.

--- Java/src/main/java/Pyramid.java
+++ Java/src/main/java/Pyramid.java
@@ -17,7 +17,28 @@ public class Pyramid {
     //Since we do not want to use that method we tell our method to override the old method.
     @Override
     public String toString() {
-        String halfPyramidToPrint = "##\n";
+        String halfPyramidToPrint = "";
+
+        for (int layerIndex = 0; layerIndex < height; layerIndex++) {
+            //This calculates the amount of hashes to have in the current printing layer.
+            //Since the initial layer will have 2 hashes we take the layer index and add two to it.
+            int hashCount = layerIndex + 2;
+            //To create the half pyramid form we need to have the correct number of spaces to justify the hashes correctly
+            //This is the height and you subtract the hash count minus 1
+            int spaceCount = height - (hashCount - 1);
+
+            //for loops do not need curly braces if it only needs a single line inside the loop
+            //An example of this are the next two for loops.  This only allows for cleaner code.
+            for (int spaceIndex = 0; spaceIndex < spaceCount; spaceIndex++)
+                halfPyramidToPrint += " ";
+
+            for (int hashIndex = 0; hashIndex < hashCount; hashIndex++)
+                halfPyramidToPrint += "#";
+
+            //This is to go to the next line of the layer.
+            halfPyramidToPrint += "\n";
+        }
+
         return halfPyramidToPrint;
     }
 
