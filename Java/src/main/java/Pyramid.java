3051fd3389565f37696ddd039169e90567081e74
sean.robb <sean.robb@centurylink.com>
2015-08-29 13:10:16 -0500

    Negative number causes an exception

--- Java/src/main/java/Pyramid.java
+++ Java/src/main/java/Pyramid.java
@@ -3,8 +3,15 @@
  */
 public class Pyramid {
 
-    public Pyramid(int height) throws Exception {
+    private int height;
 
+    public Pyramid(int height) throws Exception {
+        if (height > 0) {
+            this.height=height;
+        }
+        else {
+            throw new Exception("The pyramid height must be a non negative number and no greater then 23.");
+        }
     }
 
 
